38b7a29a55d547e7e756d973a56e39bd2539bfc2
Bowen <wem7wang@gmail.com>
2026-05-03 22:59:46 +1000

    feat: add initial movie app components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+// import MovieCard from "../components/MovieCard";
+
+// function Home() {
+//   const movie = {
+//     id: 1,
+//     title: "Test Movie",
+//     release_date: "2024-01-01",
+//     poster_path: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+//   };
+  
+
+//   return (
+//     <div className="home">
+//       <MovieCard movie={movie} />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+    const movie = {
+        id: 1,
+        title: "Test Movie",
+        release_date: "2024-01-01",
+        poster_path: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    };
+
+    return (
+        <div className="home">
+            <MovieCard movie={movie} />
+        </div>
+    );
+}
+
+export default Home;
