b95ec7f0034d927c65f80e36b0a837404f091b6e
Bowen <wem7wang@gmail.com>
2026-05-04 22:10:45 +1000

    feat: add TMDB config and popular movies loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,29 @@
 import "../css/Home.css";
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
-import { searchMovies } from "../services/api";
+import { useState, useEffect } from "react";
+import { getPopularMovies, searchMovies } from "../services/api";
 
 function Home() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Test Movie",
-      release_date: "2024-01-01",
-      poster_path: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-    },
-    {
-      id: 2,
-      title: "Another Movie",
-      release_date: "2023-05-10",
-      poster_path: "/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies); // update movies
+      } catch (err) {
+        console.log("Error fetching popular movies:", err);
+        setError("Failed to load popular movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = async (e) => {
     // Handle form submit and prevent page refresh
@@ -39,6 +42,7 @@ function Home() {
     setLoading(true);
 
     try {
+      // searching movies
       const searchResults = await searchMovies(searchQuery);
       setMovies(searchResults);
       setError(null); // Clear any previous errors
