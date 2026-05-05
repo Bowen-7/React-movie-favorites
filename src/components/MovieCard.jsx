b4ba95e6779c9426e3c295dd55bb6de65d689ef5
Bowen <wem7wang@gmail.com>
2026-05-05 22:54:35 +1000

    feat: connect favorites context to movie card and favorites page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,20 @@
 import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({ movie }) {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+    e.preventDefault(); // Prevents the default button behavior
+
+    if (favorite) {
+      removeFromFavorites(movie.id);
+    } else {
+      addToFavorites(movie);
+    }
+  }
+
   return (
     <div className="movie-card">
       <div className="movie-poster">
@@ -9,7 +23,11 @@ function MovieCard({ movie }) {
           alt={movie.title}
         />
         <div className="movie-overlay">
-          <button className="favorite-btn">♥</button>
+          <button className={`favorite-btn ${favorite ? "active" : ""}`}
+          onClick={onFavoriteClick}>
+            {/* Toggle favorite status on click */}
+            ♥
+          </button>
         </div>
       </div>
 
